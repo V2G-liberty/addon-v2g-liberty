084d26628dc4fb8d46d2dca41e9be72f5ea2b2d0
Ronald Pijnacker <ronald.pijnacker@gmail.com>
2024-11-02 22:40:46 +0000

    Simplify tests with fixtures

--- v2g-liberty/rootfs/root/appdaemon/tests/test_settings_manager.py
+++ v2g-liberty/rootfs/root/appdaemon/tests/test_settings_manager.py
@@ -1,85 +1,84 @@
+import pytest
 from unittest.mock import ANY, Mock, mock_open, patch
 from apps.v2g_liberty.settings_manager import SettingsManager
+
+@pytest.fixture
+def log_mock():
+    return Mock()
+
+@pytest.fixture
+def settings_manager(log_mock):
+    return SettingsManager(log=log_mock)
+
+@pytest.fixture
+def json_dump_mock():
+    return Mock()
+
 
 class TestRetrieveSettings:
 
     @patch('os.path.exists', lambda _: False)
-    def test_retrieve_initial_settings(self):
-        # Arrange
-        log = Mock()
-        settings = SettingsManager(log=log)
+    def test_retrieve_initial_settings(self, log_mock, settings_manager):
         # Act
-        settings.retrieve_settings()
+        settings_manager.retrieve_settings()
         # Assert
-        log.assert_called_with("retrieve_settings, no settings file found")
-        assert settings.settings == {}
+        log_mock.assert_called_with("retrieve_settings, no settings file found")
+        assert settings_manager.settings == {}
 
     @patch('os.path.exists', lambda _: True)
     @patch('builtins.open', mock_open(read_data='[]'))
-    def test_non_dict_settings(self):
-        # Arrange
-        log = Mock()
-        settings = SettingsManager(log=log)
+    def test_non_dict_settings(self, log_mock, settings_manager):
         # Act
-        settings.retrieve_settings()
+        settings_manager.retrieve_settings()
         # Assert
-        log.assert_called_with("retrieve_settings, loading file content error, no dict: '[]'.")
-        assert settings.settings == {}
+        log_mock.assert_called_with("retrieve_settings, loading file content error, no dict: '[]'.")
+        assert settings_manager.settings == {}
 
     @patch('os.path.exists', lambda _: True)
     @patch('builtins.open', mock_open(read_data='{"key":"value"}'))
-    def test_existing_settings(self):
-        # Arrange
-        log = Mock()
-        settings = SettingsManager(log=log)
+    def test_existing_settings(self, settings_manager):
         # Act
-        settings.retrieve_settings()
+        settings_manager.retrieve_settings()
         # Assert
-        assert settings.settings == {"key": "value"}
+        assert settings_manager.settings == {"key": "value"}
 
 
 @patch('builtins.open', mock_open())
-def test_store_setting():
+def test_store_setting(settings_manager, json_dump_mock):
     # Arrange
-    settings = SettingsManager(log=Mock())
-    dump_mock = Mock()
-    with patch('json.dump', dump_mock):
+    with patch('json.dump', json_dump_mock):
         # Act
-        settings.store_setting('entity_id', 'value')
+        settings_manager.store_setting('entity_id', 'value')
     # Assert
-    assert settings.settings == {"entity_id": "value"}
-    dump_mock.assert_called_with({"entity_id": "value"}, ANY)
+    assert settings_manager.settings == {"entity_id": "value"}
+    json_dump_mock.assert_called_with({"entity_id": "value"}, ANY)
 
 
-def test_reset():
+def test_reset(settings_manager, json_dump_mock):
     # Arrange
-    settings = SettingsManager(log=Mock())
-    settings.settings = {"entity_id": "value"}
-    dump_mock = Mock()
-    with patch('json.dump', dump_mock):
+    settings_manager.settings = {"entity_id": "value"}
+    with patch('json.dump', json_dump_mock):
         # Act
-        settings.reset()
+        settings_manager.reset()
     # Assert
-    assert settings.settings == {}
-    dump_mock.assert_called_with({}, ANY)
+    assert settings_manager.settings == {}
+    json_dump_mock.assert_called_with({}, ANY)
 
 
 class TestGet:
 
-    def test_get_existing(self):
+    def test_get_existing(self, settings_manager):
         # Arrange
-        settings = SettingsManager(log=Mock())
-        settings.settings = {"entity_id": "value"}
+        settings_manager.settings = {"entity_id": "value"}
         # Act
-        value = settings.get("entity_id")
+        value = settings_manager.get("entity_id")
         # Assert
         assert value == "value"
 
-    def test_get_missing(self):
+    def test_get_missing(self, settings_manager):
         # Arrange
-        settings = SettingsManager(log=Mock())
-        settings.settings = {"entity_id": "value"}
+        settings_manager.settings = {"entity_id": "value"}
         # Act
-        value = settings.get("missing_entity_id")
+        value = settings_manager.get("missing_entity_id")
         # Assert
         assert value == None
