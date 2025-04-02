ab0723e034f37eead5ab2c00e9b4c50c2dbbed8b
Felix Claessen <30658763+Flix6x@users.noreply.github.com>
2025-04-02 16:40:00 +0200

    Pass the SOC sensor ID in the flex-model so that the soc-schedules get recorded in FlexMeasures
    This also means that you should be able to then replay the SOC schedules in the FlexMeasures UI, which will help with understanding schedules and/or debugging flex-models or scheduler implementations.

--- v2g-liberty/rootfs/root/appdaemon/apps/v2g_liberty/fm_client.py
+++ v2g-liberty/rootfs/root/appdaemon/apps/v2g_liberty/fm_client.py
@@ -653,6 +653,7 @@ class FMClient:
             "roundtrip-efficiency": c.ROUNDTRIP_EFFICIENCY_FACTOR,
             "consumption-capacity": max_consumption_power_ranges,
             "production-capacity": max_production_power_ranges,
+            "state-of-charge": {"sensor": c.FM_ACCOUNT_SOC_SENSOR_ID},
         }
         self.__log(f"flex_model: {flex_model}.")
         schedule = {}
