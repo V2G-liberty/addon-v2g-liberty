6dd25fbb44396efe36806ced06e92cf1a09fbfe8
ArdJonker <github@positive-design.nl>
2025-12-19 12:39:04 +0000

    Cleanedup settingsmanager as all user have upgraded to a version > 0.5. Added upgrade for charger_type.

--- v2g-liberty/rootfs/root/appdaemon/apps/v2g_liberty/settings_manager.py
+++ v2g-liberty/rootfs/root/appdaemon/apps/v2g_liberty/settings_manager.py
@@ -33,133 +33,27 @@ class SettingsManager:
                 self.__log(f"Error reading settings file: {e}", level="WARNING")
 
     def __upgrade(self, settings: dict):
-        settings = self.__upgrade_obsolete_settings(settings)
-        settings = self.__upgrade_administrator_settings_initialised(settings)
-        settings = self.__upgrade_calendar_settings_initialised(settings)
-        settings = self.__upgrade_charger_settings_initialised(settings)
-        settings = self.__upgrade_electricity_contract_settings_initialised(settings)
-        settings = self.__upgrade_schedule_settings_initialised(settings)
+        settings = self.__upgrade_charger_type(settings)
         return settings
 
     def __upgrade_obsolete_settings(self, settings: dict):
         self.__log("Called")
-
-        # TODO: Review if this code can be removed again if all users have upgraded to
-        # version 0.5.0 (Feb 2025) or above."""
-        for obsolete, new in {
-            "input_select.admin_mobile_name": "input_text.admin_mobile_name",
-            "input_select.fm_asset": "input_text.fm_asset",
-            "input_select.integration_calendar_entity_name": "input_text.integration_calendar_entity_name",
-            "input_select.car_calendar_source": "input_text.car_calendar_source",
-            "input_select.car_calendar_name": "input_text.car_calendar_name",
-        }.items():
-            if obsolete in settings:
-                self.__log(f"Changed obsolete setting key: {obsolete} to {new}.")
-                value = settings.get(obsolete)
-                settings.update({new: value})
-                settings.pop(obsolete)
-
-        # TODO: Review if this code can be removed again if all users have upgraded to
-        # version 0.5.0 (Feb 2025) or above."""
-        setting_key = "input_text.car_calendar_source"
-        for obsolete, new in {
-            "Home Assistant integration": "localIntegration",
-            "Direct caldav source": "remoteCaldav",
-        }.items():
-            value = settings.get(setting_key)
-            if value == obsolete:
-                self.__log(f"Changed setting ({setting_key}): {obsolete} to {new}.")
-                settings.update({setting_key: new})
-
-        # TODO: Review if this code can be removed again if all users have upgraded to
-        # version 0.5.3 (March 2025) or above."""
-        setting_key = "input_text.fm_host_url"
-        value = settings.get(setting_key)
-        if value == "https://seita.energy":
-            self.__log(
-                f"Changed ({setting_key}): 'https://seita.energy' to 'https://ems.seita.energy'."
-            )
-            settings.update({setting_key: "https://ems.seita.energy"})
-
+        # Keep for now eventhough it is un used now, in later versions there might be a need for this
         return settings
 
-    def __upgrade_administrator_settings_initialised(self, settings: dict):
-        if (
-            "input_text.admin_mobile_name" in settings
-            and "input_select.admin_mobile_platform" in settings
-        ):
-            settings["input_boolean.admin_settings_initialised"] = True
-        return settings
-
-    def __upgrade_calendar_settings_initialised(self, settings: dict):
-        source = settings.get("input_text.car_calendar_source", None)
-        if (
-            source == "remoteCaldav"
-            and "input_text.calendar_account_init_url" in settings
-            and "input_text.calendar_account_username" in settings
-            and "input_text.calendar_account_password" in settings
-            and "input_text.car_calendar_name" in settings
-        ) or (
-            source == "localIntegration"
-            and "input_text.integration_calendar_entity_name" in settings
-        ):
-            settings["input_boolean.calendar_settings_initialised"] = True
-        return settings
-
-    def __upgrade_charger_settings_initialised(self, settings: dict):
+    def __upgrade_charger_type(self, settings: dict):
+        # It is safe to assume charger will be of type "wallbox-quasar-1" if currenttly the
+        # URL, port and reduce boolean are present.
         if (
             "input_text.charger_host_url" in settings
             and "input_number.charger_port" in settings
             and "input_boolean.use_reduced_max_charge_power" in settings
+            and "input_text.charger_type" not in settings
         ):
-            settings["input_boolean.charger_settings_initialised"] = True
-        return settings
+            # settings["input_boolean.charger_settings_initialised"] = True
+            settings["input_text.charger_type"] = "wallbox-quasar-1"
+            self.__log("Assuming charger_type to be 'wallbox-quasar-1'.")
 
-    def __upgrade_electricity_contract_settings_initialised(self, settings: dict):
-        if "input_select.electricity_provider" in settings:
-            contract = settings["input_select.electricity_provider"]
-            if (
-                (
-                    contract
-                    in [
-                        "nl_anwb_energie",
-                        "nl_greenchoice",
-                        "nl_next_energy",
-                        "nl_tibber",
-                    ]
-                )
-                or (
-                    contract == "nl_generic"
-                    and "input_number.energy_price_vat" in settings
-                    and "input_number.energy_price_markup_per_kwh" in settings
-                )
-                or (
-                    contract == "au_amber_electric"
-                    and "input_text.own_consumption_price_entity_id" in settings
-                    and "input_text.own_production_price_entity_id" in settings
-                )
-                or (
-                    contract == "gb_octopus_energy"
-                    and "input_text.octopus_import_code" in settings
-                    and "input_text.octopus_export_code" in settings
-                    and "input_select.gb_dno_region" in settings
-                )
-            ):
-                settings["input_boolean.electricity_contract_settings_initialised"] = (
-                    True
-                )
-        return settings
-
-    def __upgrade_schedule_settings_initialised(self, settings: dict):
-        # Earlier versions before 0.5.0 did not store the URL if the default was used.
-        # So, the input_text.fm_host_url cannot be used for testing initialisation.
-        if (
-            "input_text.fm_account_username" in settings
-            and "input_text.fm_account_password" in settings
-            and "input_boolean.fm_show_option_to_change_url" in settings
-            and "input_text.fm_asset" in settings
-        ):
-            settings["input_boolean.schedule_settings_initialised"] = True
         return settings
 
     def store_setting(self, entity_id: str, value: any):
